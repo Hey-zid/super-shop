304657957b3f3edc8f3204dd0ff5321f55962a19
Hey-zid <bayazidparvez3@gmail.com>
2025-09-09 16:14:07 +0600

    Largest number

---
+++ 3rd-Assignment.c
@@ -0,0 +1,16 @@
+//Write a Program to Swap the values of two variables.
+#include <stdio.h>
+int main()
+{
+    int ami=25 ,tumi=30 ,she;
+    she = ami;
+    ami = tumi;
+    tumi = she;
+    printf(" %d\n ",ami);
+    printf("%d",tumi);
+
+
+
+
+    return 0;
+}
